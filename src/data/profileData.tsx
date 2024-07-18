b6dbced874a330a914fd57444645595ff8e5e370
GokulKir <gokulkrishna22072003@gmail.com>
2024-07-18 21:18:12 +0530

    Initial commit

--- src/data/profileData.tsx
+++ src/data/profileData.tsx
@@ -2,7 +2,7 @@
 
 const profileData = {
     name: "Akshay",
-    email: "Akshay@example.com",
+    email: "Akshay@examplegit commit -m "first commit".com",
     bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
     avatar: require('../assets/ProfileCard.png'), // URL to profile picture
   };
